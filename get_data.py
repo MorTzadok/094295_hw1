da8c2bfa1935f6a869996f4b54e68aa0d4d59194
97250 <xdfghjm>
2023-04-30 19:34:27 +0300

    4-5

--- get_data.py
+++ get_data.py
@@ -3,23 +3,23 @@ import numpy
 import csv
 import os
 
-
-files = os.listdir('data/train')
-to_csv = []
-for file in range(0, len(files)):
-    path_name = os.path.abspath(os.path.join('data/train',files[file]))
-    to_csv.append(path_name)
-flag=True
-for i,one_file in enumerate(to_csv):
-    with open(one_file, "r", encoding="utf-8") as f:
-        if flag == True:
-            patient_df = pd.read_csv(one_file,sep='|')
-            patient_df['patient'] = str(i)
-            flag = False
-        else:
-            new_patient_df = pd.read_csv(one_file, sep='|')
-            new_patient_df['patient'] = str(i)
-            patient_df = pd.concat([patient_df,new_patient_df], ignore_index = True)
-patient_df.to_csv('patient_df.csv')
+def load_data():
+    files = os.listdir('data/train')
+    to_csv = []
+    for file in range(0, len(files)):
+        path_name = os.path.abspath(os.path.join('data/train',files[file]))
+        to_csv.append(path_name)
+    flag=True
+    for i,one_file in enumerate(to_csv):
+        with open(one_file, "r", encoding="utf-8") as f:
+            if flag == True:
+                patient_df = pd.read_csv(one_file,sep='|')
+                patient_df['patient'] = str(i)
+                flag = False
+            else:
+                new_patient_df = pd.read_csv(one_file, sep='|')
+                new_patient_df['patient'] = str(i)
+                patient_df = pd.concat([patient_df,new_patient_df], ignore_index = True)
+    return patient_df.to_csv('patient_df.csv')
 
 
